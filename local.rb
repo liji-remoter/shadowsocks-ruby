6c6cbdaa88160b75b608a1fd9a3874b9074b4b15
Li Ji <liji@remoter.club>
2017-09-30 16:56:29 +0800

    Improvement code

--- local.rb
+++ local.rb
@@ -1,35 +1,86 @@
 require "socket"
-require "pry"
+require 'openssl'
+require 'pry'
 
+# local_ip = 'my-local-ip-address'
+# local_port = 'my-local-port'
+# server_ip = 'my-server-ip-address'
+# server_port = 'my-server-port'
 local_ip = '127.0.0.1'
-local_port = '1080'
-server_ip = '192.168.1.5'
-server_port = '8499'
+local_port = 2000
+server_ip = '188.166.24.200'
+server_port = 8499
+key = "625bd76ee1934ad1b53f22c8d0222738"
+iv = "c6f97771e3dfd066"
+package_size = 8192
+
+encrypt_data =
+  Proc.new do |plain_data|
+    if plain_data.empty?
+      plain_data
+    else
+      cipher = OpenSSL::Cipher::AES256.new(:CFB)
+      cipher.encrypt
+      cipher.key = key
+      cipher.iv = iv
+      encrypted_data = cipher.update(plain_data) + cipher.final
+      data_length = [encrypted_data.length.to_s(16).rjust(4, '0')].pack("H*")
+      data_length + encrypted_data
+    end
+  end
+
+decrypt_data =
+  Proc.new do |encrypted_data|
+    if encrypted_data.empty?
+      encrypted_data
+    else
+      decipher = OpenSSL::Cipher::AES256.new(:CFB)
+      decipher.decrypt
+      decipher.key = key
+      decipher.iv = iv
+      decipher.update(encrypted_data) + decipher.final
+    end
+  end
 
 handle_tcp =
-  Proc.new do |sock, remote|
-    fdset = [sock, remote]
+  Proc.new do |local_connection, remote_connection|
+    fdset = [local_connection, remote_connection]
     counter = 0
     while true
-      r, w, e = IO.select([sock, remote], [], [])
+      r, w, e = IO.select(fdset, [], [])
 
-      if r.include?(sock)
-        r_data = sock.recv(4096)
+      if r.include?(local_connection)
+        recv_data = local_connection.recv(package_size)
         if counter == 1
           begin
-            addr = r_data[4..-3]
+            addr = recv_data[5..-3]
             puts "Connecting #{addr}"
           rescue Exception => e
             nil
           end
         end
         counter += 1 if counter < 2
-        break if remote.send(r_data, 0) <= 0
+        break if remote_connection.send(encrypt_data.call(recv_data), 0) <= 0
       end
 
-      if r.include?(remote)
-        data = remote.recv(4096)
-        break if sock.send(data, 0) <= 0
+      if r.include?(remote_connection)
+        data_length = remote_connection.recv(2).unpack("H*").first.hex
+        puts "data_length: #{data_length}"
+        if data_length > 0
+          remote_data = ""
+          while (remote_data_length = remote_data.length) < data_length
+            if remote_data_length == 0
+              remote_data += remote_connection.recv(data_length)
+            else
+              remote_data += remote_connection.recv(data_length - remote_data_length)
+            end
+          end
+          remote_data = decrypt_data.call(remote_data)
+          puts "remote_data length: #{remote_data.length}"
+        else
+          remote_data = ""
+        end
+        break if local_connection.send(remote_data, 0) <= 0
       end
     end
   end
--- server.rb
+++ server.rb
@@ -1,27 +1,93 @@
 require "socket"
 require "ipaddr"
-require 'pry'
+require "openssl"
+require "pry"
 
-port = "8499"
-server = "192.168.1.5"
+# port = "my-server-port"
+# server = "my-server-ip-address"
+port = 8499
+server = "188.166.24.200"
+key = "625bd76ee1934ad1b53f22c8d0222738"
+iv = "c6f97771e3dfd066"
+package_size = 8192
+
+encrypt_data =
+  Proc.new do |plain_data|
+    if plain_data.empty?
+      plain_data
+    else
+      cipher = OpenSSL::Cipher::AES256.new(:CFB)
+      cipher.encrypt
+      cipher.key = key
+      cipher.iv = iv
+      encrypted_data = cipher.update(plain_data) + cipher.final
+      data_length = [encrypted_data.length.to_s(16).rjust(4, '0')].pack("H*")
+      data_length + encrypted_data
+    end
+  end
+
+decrypt_data =
+  Proc.new do |encrypted_data|
+    if encrypted_data.empty?
+      encrypted_data
+    else
+      decipher = OpenSSL::Cipher::AES256.new(:CFB)
+      decipher.decrypt
+      decipher.key = key
+      decipher.iv = iv
+      decipher.update(encrypted_data) + decipher.final
+    end
+  end
 
 handle_tcp =
   Proc.new do |sock, remote|
     fdset = [sock, remote]
-
     while true
-      r, w, e = IO.select([sock, remote], [], [])
-      r.include?(sock) && remote.send(sock.recv(4096), 0) <= 0 && break
-      r.include?(remote) && sock.send(remote.recv(4096), 0) <= 0 && break
+      r, w, e = IO.select(fdset, [], [])
+      if r.include?(sock)
+        begin
+          data_length = sock.recv(2).unpack("H*").first.hex
+          if data_length > 0
+            recv_data = ""
+            while (recev_data_length = recv_data.length) < data_length
+              if recev_data_length == 0
+                recv_data += sock.recv(data_length)
+              else
+                recv_data += sock.recv(data_length - recev_data_length)
+              end
+            end
+            recv_data = decrypt_data.call(recv_data)
+          else
+            recv_data = ""
+          end
+          if remote.send(recv_data, 0) <= 0
+            break
+          end
+        rescue Exception => e
+          puts e
+        end
+      end
+      if r.include?(remote)
+        begin
+          x = remote.recv(package_size)
+          puts "remote recv length: #{x.length}"
+          puts "encrypted remote recv length: #{encrypt_data.call(x).length}"
+          if sock.send(encrypt_data.call(x), 0) <= 0
+            break
+          end
+        rescue Exception => e
+          puts e
+        end
+      end
     end
   end
 
 handle =
   Proc.new do |sock|
-    x = sock.recv(4096)
-    puts x.unpack("H*")
-    sock.send("\x05\x00", 0)
-    data = sock.recv(4096)
+    recv_data = sock.recv(package_size)
+    sock.send(encrypt_data.call("\x05\x00"), 0)
+    data_length = sock.recv(2).unpack("H*").first.hex
+    data = decrypt_data.call(sock.recv(data_length))
     mode = data[1].unpack("H*").first.hex
     addrtype = data[3].unpack("H*").first.hex
     if addrtype == 1
@@ -31,27 +97,28 @@ handle =
     else
       return
     end
-
     port = data[-2,2].unpack("H*").first.hex
     reply = "\x05\x00\x00\x01"
 
     begin
       if mode == 1
-        remote = TCPSocket.new(addr, port)
+        # remote = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+        # remote.connect Socket.pack_sockaddr_in(port, addr)
+        remote = TCPSocket.new(addr, port, Socket::AF_INET)
         local_address = remote.local_address
         packed_address = [IPAddr.new(local_address.ip_address).to_i.to_s(16)].pack("H*")
         packed_port = [local_address.ip_port.to_s(16).rjust(4, '0')].pack("H*")
         reply += packed_address + packed_port
         puts "Tcp connect to #{addr} #{port}"
       else
-        reply = "\x05\x05\x00\x01\x00\x00\x00\x00\x00\x00"
+        reply = "\x05\x05\x00\x01"
       end
-      sock.send(reply, 0)
-      handle_tcp.call(sock, remote) if reply[1] == "\x00" && mode == 1
-      remote.close
     rescue Exception => e
-      puts e.backtrace
+      reply = "\x05\x05\x00\x01\x00\x00\x00\x00\x00\x00"
     end
+    sock.send(encrypt_data.call(reply), 0)
+    handle_tcp.call(sock, remote) if reply[1] == "\x00" && mode == 1
+    remote.close
   end
 
 server = TCPServer.new(server, port)
